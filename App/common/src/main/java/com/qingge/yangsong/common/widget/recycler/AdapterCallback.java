b0b0cd4da84a3e86d347e66c32cee0e0823839d5
ysjava <ysjava@test.com>
2019-04-01 12:07:04 +0800

    重新放置项目

--- App/common/src/main/java/com/qingge/yangsong/common/widget/recycler/AdapterCallback.java
+++
@@ -1,5 +0,0 @@
-package com.qingge.yangsong.common.widget.recycler;
-
-public interface AdapterCallback<Data>  {
-    void update(Data data,RecyclerAdapter.ViewHolder<Data> holder);
-}
--- App/common/src/main/java/com/qingge/yangsong/factory/model/Author.java
+++
@@ -1,9 +0,0 @@
-package com.qingge.yangsong.factory.model;
-public interface Author {
-    String getId();
-    void setId(String id);
-    String getName();
-    void setName(String name);
-    String getPortrait();
-    void setPortrait(String portrait);
-}
